f99e58495ff3573f6e3627ad544102ffdad3ad05
Richard Fonfára <17224380+rischo32@users.noreply.github.com>
2026-02-28 15:36:32 +0100

    Update main.js
    Signed-off-by: Richard Fonfára <17224380+rischo32@users.noreply.github.com>

--- vortex/js/main.js
+++ vortex/js/main.js
@@ -1,31 +1,77 @@
+import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
+import { EffectComposer } from 'https://unpkg.com/three@0.158.0/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://unpkg.com/three@0.158.0/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'https://unpkg.com/three@0.158.0/examples/jsm/postprocessing/UnrealBloomPass.js';
+
 import { SceneManager } from './scene.js';
 import { InputManager } from './input.js';
 
 class VortexApp {
+
   constructor() {
+
     this.sceneManager = new SceneManager();
     this.inputManager = new InputManager(this.sceneManager);
+
+    this.composer = null;
+
+    this.init();
   }
 
   init() {
+
     this.sceneManager.init();
     this.inputManager.init();
-    this.animate();
+
+    this.setupPostProcessing();
     this.setupResize();
+    this.animate();
+  }
+
+  setupPostProcessing() {
+
+    const { renderer, scene, camera } = this.sceneManager;
+
+    this.composer = new EffectComposer(renderer);
+
+    const renderPass = new RenderPass(scene, camera);
+    this.composer.addPass(renderPass);
+
+    const bloomPass = new UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      1.1,  // strength
+      0.3,  // radius
+      0.85  // threshold
+    );
+
+    this.composer.addPass(bloomPass);
   }
 
   animate() {
+
     requestAnimationFrame(() => this.animate());
+
     this.sceneManager.update();
-    this.sceneManager.render();
+
+    if (this.composer) {
+      this.composer.render();
+    }
   }
 
   setupResize() {
+
     window.addEventListener('resize', () => {
+
       this.sceneManager.onResize();
+
+      if (this.composer) {
+        this.composer.setSize(
+          window.innerWidth,
+          window.innerHeight
+        );
+      }
     });
   }
 }
 
-const app = new VortexApp();
-app.init();
+new VortexApp();
