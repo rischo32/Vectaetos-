5857c8b5d5efe23e2336bfacf1c33521b3eb65af
Richard Fonfára <17224380+rischo32@users.noreply.github.com>
2026-03-01 16:18:55 +0100

    Update main.js
    Signed-off-by: Richard Fonfára <17224380+rischo32@users.noreply.github.com>

--- vortex/js/main.js
+++ vortex/js/main.js
@@ -1,19 +1,29 @@
+/* =========================
+   CDN IMPORTS (NO BUNDLER)
+========================= */
+
 import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
-import { EffectComposer } from 'https://unpkg.com/three@0.158.0/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://unpkg.com/three@0.158.0/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'https://unpkg.com/three@0.158.0/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js';
+
+/* =========================
+   INTERNAL MODULES
+========================= */
 
 import { SceneManager } from './scene.js';
 import { InputManager } from './input.js';
+
+/* =========================
+   APP CORE
+========================= */
 
 class VortexApp {
 
   constructor() {
 
+    console.log("VORTEX MAIN LOADED");
+
     this.sceneManager = new SceneManager();
     this.inputManager = new InputManager(this.sceneManager);
-
-    this.composer = null;
 
     this.init();
   }
@@ -23,28 +33,8 @@ class VortexApp {
     this.sceneManager.init();
     this.inputManager.init();
 
-    this.setupPostProcessing();
-    this.setupResize();
     this.animate();
-  }
-
-  setupPostProcessing() {
-
-    const { renderer, scene, camera } = this.sceneManager;
-
-    this.composer = new EffectComposer(renderer);
-
-    const renderPass = new RenderPass(scene, camera);
-    this.composer.addPass(renderPass);
-
-    const bloomPass = new UnrealBloomPass(
-      new THREE.Vector2(window.innerWidth, window.innerHeight),
-      1.1,  // strength
-      0.3,  // radius
-      0.85  // threshold
-    );
-
-    this.composer.addPass(bloomPass);
+    this.setupResize();
   }
 
   animate() {
@@ -52,26 +42,18 @@ class VortexApp {
     requestAnimationFrame(() => this.animate());
 
     this.sceneManager.update();
-
-    if (this.composer) {
-      this.composer.render();
-    }
   }
 
   setupResize() {
 
     window.addEventListener('resize', () => {
-
       this.sceneManager.onResize();
-
-      if (this.composer) {
-        this.composer.setSize(
-          window.innerWidth,
-          window.innerHeight
-        );
-      }
     });
   }
 }
+
+/* =========================
+   START
+========================= */
 
 new VortexApp();
